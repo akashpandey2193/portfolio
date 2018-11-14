a476bad4d2cc36c7b09c8ac5258114fac5278da0
Akash Pandey <akashpandey2193@gmail.com>
2018-11-14 21:28:21 +0530

    remove unnecessary stuff add footer

---
+++ components/common/footer/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-footer
+    dark
+    height="auto">
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+        <v-spacer/>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-3"
+          dark
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+      <v-card-actions class="grey darken-3 justify-center">
+        &copy;2018
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      icons: [
+       'fab fa-facebook',
+       'fab fa-twitter',
+       'fab fa-google-plus',
+       'fab fa-linkedin',
+       'fab fa-instagram'
+     ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
