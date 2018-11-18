d58434cc8c314dc3c6cac676222f0742ee2706df
Akash Pandey <akashpandey2193@gmail.com>
2018-11-18 17:55:12 +0530

    add header and complete the top of the portfolio

---
+++ components/common/header/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="top-nav">
+    <v-toolbar
+      height="60px"
+      fixed>
+      <v-spacer/>
+      <v-toolbar-items>
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,69 @@
 <template>
   <v-layout
     column>
-    <div>Akash</div>
+    <header-comp/>
+    <div class="profile-section">
+      <v-flex
+        sm4
+        lg2>
+        <div class="profile-image"/>
+      </v-flex>
+      <v-flex
+        sm8
+        lg10
+        class="pa-4">
+        <div class="display-3">AKASH PANDEY</div>
+        <div class="general-info font-weight-light">
+          <div class="title pb-2">
+            <span class="label">Current Designation: </span>
+            <span class="label-info">Software Developer at CatchThatBus</span>
+          </div>
+          <div class="title py-2">
+            <span class="label">Location: </span>
+            <span class="label-info">Hyderabad,Telangana</span>
+          </div>
+          <div class="title py-2">
+            <span class="label">About Me:</span>
+            <span
+              class="label-info">
+              An enthusiastic individual with leadership skills. Can easily adapt to change and possesses
+              eagerness towards learning and contributing to the organization.
+            </span>
+          </div>
+        </div>
+
+      </v-flex>
+    </div>
     <footer-comp/>
   </v-layout>
 </template>
 
 <script>
 import FooterComp from '@/components/common/footer'
+import HeaderComp from '@/components/common/header'
 export default {
   components: {
-    FooterComp
+    FooterComp,
+    HeaderComp
   }
 }
 </script>
 
 <style scoped lang="stylus">
+.profile-section
+  margin-top 60px
+  height 50vh
+  background white
+  position relative
+  display flex
+.profile-image
+  height 100%
+  width 100%
+  background url('/profile.jpg') no-repeat
+  background-size contain
+.general-info
+  >>>.label
+      font-style italic
+  >>>.label-info
+      font-weight 400
 </style>
